f6d748f0a1f7d95081835f03830572cd7aa16eb3
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-08-06 16:10:24 +0200

    feat: create infinite slider

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,26 +1,71 @@
-import Image from "next/image";
+"use client";
+
+import Image from 'next/image';
 
 const Footer = () => {
+
     return (
         <>
-            <section>
-                <div className="swiper">
-                    <div className="swiper-wrapper bg-dark text-white font-yipes">
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1"><Image width={22} height={32} src="/images/swiper-star.png" alt="étoile" /></div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1">Disponible pour du freelance</div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1"><Image width={22} height={32} src="/images/swiper-star.png" alt="étoile" /></div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1">Disponible pour du freelance</div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1"><Image width={22} height={32} src="/images/swiper-star.png" alt="étoile" /></div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1">Disponible pour du freelance</div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1"><Image width={22} height={32} src="/images/swiper-star.png" alt="étoile" /></div>
-                        <div className="swiper-slide font-yipes font-sm px-6 py-1">Disponible pour du freelance</div>
+            <div className="slider">
+                <div className="slide-track">
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
+                    </div>
+                    <div className="slide">
+                        <Image className="p-2" width={38} height={48} src="/images/swiper-star.png" alt="étoile" />
+                        <p className="font-yipes font-sm px-6 py-1 text-white">Disponible pour du freelance</p>
                     </div>
                 </div>
-            </section>
+            </div>
             <footer id="contact" className="flex flex-col items-center bg-cream">
-                <h2 className="font-yipes text-2xl py-32">Comment puis-je vous aidez ?</h2>
+                <h2 className="font-yipes text-2xl py-32">Comment puis-je vous aider ?</h2>
                 <div className="flex flex-col gap-4 items-start w-full px-56 pb-20">
-                    <p className="text-xs max-w-[739px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent  ultricies odio et massa semper, ut placerat nisi posuere. Sed euismod  justo sit amet lorem hendrerit, non eleifend diam sodales.</p>
+                    <p className="text-xs max-w-[739px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies odio et massa semper, ut placerat nisi posuere. Sed euismod justo sit amet lorem hendrerit, non eleifend diam sodales.</p>
                     <a href="mailto:#" className="font-bold text-xs underline">Pour m'écrire un email de fifou</a>
                 </div>
                 <div className="w-full border-t border-dark"></div>
