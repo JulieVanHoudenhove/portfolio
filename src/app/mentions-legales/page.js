d2c2d850502cb1b0a1359b2b1f2ae550f22e5e0b
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-08-09 17:35:17 +0200

    feat: create legal notice page and sitemap

---
+++ src/app/mentions-legales/page.js
@@ -0,0 +1,16 @@
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+const MentionsLegales = () => {
+    return (
+        <>
+            <Header />
+            <section className="bg-cream pt-[152px]">
+                Blop
+            </section>
+            <Footer />
+        </>
+    );
+}
+
+export default MentionsLegales;
