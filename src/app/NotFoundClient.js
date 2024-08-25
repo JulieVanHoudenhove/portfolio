67101088c311a60f21a6cf10921e96dd0d1262ed
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-08-25 21:03:26 +0200

    feat: add animations

--- src/app/NotFoundClient.js
+++ src/app/NotFoundClient.js
@@ -1,17 +1,37 @@
-"use client"
+"use client";
 
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import Link from "next/link";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function NotFoundClient() {
+    useEffect(() => {
+        // Animation des éléments au chargement de la page
+        gsap.fromTo(
+            ['.notfound-title', '.notfound-message', '.notfound-link'],
+            { opacity: 0, y: -50 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                stagger: 0.5, // Décale les animations des éléments les uns après les autres
+                ease: 'power2.out'
+            }
+        );
+    }, []);
+
     return (
         <>
             <Header />
             <div className="min-h-screen flex flex-col items-center justify-center bg-cream">
-                <h1 className="flex justify-center font-yipes text-xl lg:text-2xl font-bold">404</h1>
-                <p className="text-lg lg:text-xl mb-8">Oups ! Page non trouvée</p>
-                <Link href="/" className="font-bold text-2xs lg:text-xs underline hover:opacity-70 transition">Retourner à l'accueil</Link>
+                <h1 className="notfound-title flex justify-center font-yipes text-xl lg:text-2xl font-bold">404</h1>
+                <p className="notfound-message text-lg lg:text-xl mb-8">Oups ! Page non trouvée</p>
+                <Link href="/" className="notfound-link font-bold text-2xs lg:text-xs underline hover:opacity-70 transition">Retourner à l'accueil</Link>
             </div>
             <Footer />
         </>
