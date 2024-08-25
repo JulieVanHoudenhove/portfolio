16cd9ed5e18b73138a723fe0a4ea60c15493419b
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-08-25 15:21:28 +0200

    refactor: add icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/images/favicon_star.svg" type="image/x-icon" sizes="16x16"/>
+            </head>
             <body>
                 <Cursor />
                 {children}
